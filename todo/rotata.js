091eaca80c02196766edbeb412abf3b8138e713d
Dwan <yingduan_ge@epam.com>
2023-05-04 18:23:58 +0800

    39. combination sum

---
+++ todo/rotata.js
@@ -0,0 +1,10 @@
+const rotate = function(matrix) {
+	for(let i = 0;i < matrix.length;i++){
+		for(let j = 0;j < matrix.length;j++){
+		}
+	}
+	return matrix;
+};
+const matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]];
+ 
+console.log(rotate(matrix));
