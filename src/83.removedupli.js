8427419541869d9974cfc49913b07d3d5d5d0700
Dwan <yingduan_ge@epam.com>
2023-05-30 14:49:44 +0800

    remove duplicates

---
+++ src/83.removedupli.js
@@ -0,0 +1,21 @@
+function ListNode(val, next) {
+	this.val = (val===undefined ? 0 : val)
+	this.next = (next===undefined ? null : next)
+}
+
+const deleteDuplicates = function(head) {
+	if(!head) return null;
+	const ans = head;
+	while(head.next){
+		if(head.val === head.next.val){
+			head.next = head.next.next;
+		}else{
+			head = head.next;
+		}
+	}
+	return ans;
+};
+
+const head = new ListNode(1, new ListNode(2, new ListNode(2)));
+
+console.log(deleteDuplicates(head));
